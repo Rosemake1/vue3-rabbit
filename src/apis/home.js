1047b7a7462726ad34ff9599824e8b561226e1d9
李欣枝 <1113415834@qq.com>
2024-04-06 22:14:05 +0800

    home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,9 +1,16 @@
 import httpInstace from "@/utils/http";
 
+
 //获取banner
 export function getBannerAPI(){
     return  httpInstace({
         url:'/home/banner'
     })
 
+}
+
+export const findNewAPI=()=>{
+        return httpInstace({
+            url:'home/new'
+        })
 }
